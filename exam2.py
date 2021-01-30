a17df003bd06698829221e5c21f8922baa137656
Tran Thu Le <letranthu23041996@gmail.com>
2021-01-30 10:49:02 +0100

    Create exam2.py

---
+++ exam2.py
@@ -0,0 +1,12 @@
+from exam1 import my_object
+
+
+if __name__ == '__main__':
+    import torch
+    import numpy as np
+    import matplotlib.pyplot as plt
+    print("torch, numpy, mathplotlib have been imported successfully")
+    
+    a=my_object()
+    a.show()
+    print("Demo: exam2 was run successfully")
