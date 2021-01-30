ad1855f1a8b8900058bd7dd258e5dc786395bfcb
Tran Thu Le <nymtiger@yahoo.com.vn>
2021-01-30 10:40:49 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+class test(object):
+    def show(self):
+        print("hello the world from test object")
